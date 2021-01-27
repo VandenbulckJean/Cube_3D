1d7e0e116dd3d6fb466ba747f4c0a872ed172f81
Jean Vanden Bulck <jeanvandenbulck@MacBook-Pro-de-Jean.local>
2021-01-27 16:40:28 +0100

    norminette03

--- srcs/main.c
+++ srcs/main.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jvanden- <jvanden-@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/01/27 16:40:26 by jvanden-          #+#    #+#             */
+/*   Updated: 2021/01/27 16:40:27 by jvanden-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../include/cube.h"
 
 static void		parsing_file(t_cube *cube)
@@ -13,16 +25,14 @@ static void		parsing_file(t_cube *cube)
 int				main(int argc, char *argv[])
 {
 	t_cube	cube;
+
 	ft_bzero(&cube, sizeof(t_cube));
 	check_arguments(argc, argv, &cube.filename);
 	if (!(cube.ptr = mlx_init()))
 		print_error_exit("fail to initialize mlx.");
 	parsing_file(&cube);
-
 	initialisation(&cube);
 	if (argc == 3)
 		make_bmp(&cube);
-	else
-		event_loop(&cube);
-	return (0);
+	event_loop(&cube);
 }
