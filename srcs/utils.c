fd952b323162624c8964d4c4c00e25e995f10e01
Jean Vanden Bulck <jeanvandenbulck@MacBook-Pro-de-Jean.local>
2021-01-27 16:39:36 +0100

    norminette03

--- srcs/utils.c
+++ srcs/utils.c
@@ -1,13 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jvanden- <jvanden-@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/01/27 16:39:34 by jvanden-          #+#    #+#             */
+/*   Updated: 2021/01/27 16:39:35 by jvanden-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../include/cube.h"
 
-int	is_space(char c)
+int			is_space(char c)
 {
 	if (c == 32 || (9 <= c && c <= 13))
 		return (1);
 	return (0);
 }
 
-int				is_empty_line(char *str)
+int			is_empty_line(char *str)
 {
 	int i;
 
@@ -28,7 +40,7 @@ int			check_if_number(char *str, int i)
 	if (str[i] && str[i] == '-')
 		i++;
 	if (str[i] && isnumber(str[i]))
-		return(0);
+		return (0);
 	return (1);
 }
 
@@ -38,29 +50,29 @@ void		atoi_cube(char *str, int *i, int *result)
 
 	*result = 0;
 	sign = 0;
-	while(str[*i] && is_space(str[*i]))
+	while (str[*i] && is_space(str[*i]))
 		(*i)++;
 	if (str[(*i)] == '-')
 	{
 		sign++;
 		(*i)++;
 	}
-	while(isnumber(str[*i]))
+	while (isnumber(str[*i]))
 	{
 		*result = *result * 10 + (str[*i] - '0');
 		(*i)++;
 		if (*result > (INT_MAX / 100))
 		{
-			while(isnumber(str[*i]))
+			while (isnumber(str[*i]))
 				(*i)++;
-			break;
+			break ;
 		}
 	}
 	if (sign)
 		*result = -(*result);
 }
 
-int		is_first_closer(t_vecteur s1, t_vecteur s2, t_vecteur pos)
+int			is_first_closer(t_vecteur s1, t_vecteur s2, t_vecteur pos)
 {
 	double dist1;
 	double dist2;
