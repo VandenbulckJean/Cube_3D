80b42b741433bde444d4175d23a5b0660b673774
Jean Vanden Bulck <jeanvandenbulck@MacBook-Pro-de-Jean.local>
2021-01-27 17:10:58 +0100

    norminette03

--- srcs/movementbis.c
+++ srcs/movementbis.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   movementbis.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jvanden- <jvanden-@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/01/27 17:10:55 by jvanden-          #+#    #+#             */
+/*   Updated: 2021/01/27 17:10:56 by jvanden-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../include/cube.h"
 
 void	rotation_left_pov(t_cube *cube)
@@ -7,10 +19,14 @@ void	rotation_left_pov(t_cube *cube)
 
 	buffdirx = cube->cam.direction.x;
 	buffplanex = cube->cam.plane.x;
-	cube->cam.direction.x = (buffdirx * cos(-Alpha)) - (cube->cam.direction.y * sin(-Alpha));
-	cube->cam.direction.y = (buffdirx * sin(-Alpha)) + (cube->cam.direction.y * cos(-Alpha));
-	cube->cam.plane.x = (buffplanex * cos(-Alpha)) - (cube->cam.plane.y * sin(-Alpha));
-	cube->cam.plane.y = (buffplanex * sin(-Alpha)) + (cube->cam.plane.y * cos(-Alpha));
+	cube->cam.direction.x = (buffdirx * cos(-Alpha)) -
+	(cube->cam.direction.y * sin(-Alpha));
+	cube->cam.direction.y = (buffdirx * sin(-Alpha)) +
+	(cube->cam.direction.y * cos(-Alpha));
+	cube->cam.plane.x = (buffplanex * cos(-Alpha)) -
+	(cube->cam.plane.y * sin(-Alpha));
+	cube->cam.plane.y = (buffplanex * sin(-Alpha)) +
+	(cube->cam.plane.y * cos(-Alpha));
 	cube->event.left_arrow_pressed = 0;
 	raycasting(cube);
 }
@@ -22,10 +38,14 @@ void	rotation_right_pov(t_cube *cube)
 
 	buffdirx = cube->cam.direction.x;
 	buffplanex = cube->cam.plane.x;
-	cube->cam.direction.x = (buffdirx * cos(Alpha)) - (cube->cam.direction.y * sin(Alpha));
-	cube->cam.direction.y = (buffdirx * sin(Alpha)) + (cube->cam.direction.y * cos(Alpha));
-	cube->cam.plane.x = (buffplanex * cos(Alpha)) - (cube->cam.plane.y * sin(Alpha));
-	cube->cam.plane.y = (buffplanex * sin(Alpha)) + (cube->cam.plane.y * cos(Alpha));
+	cube->cam.direction.x = (buffdirx * cos(Alpha)) -
+	(cube->cam.direction.y * sin(Alpha));
+	cube->cam.direction.y = (buffdirx * sin(Alpha)) +
+	(cube->cam.direction.y * cos(Alpha));
+	cube->cam.plane.x = (buffplanex * cos(Alpha)) -
+	(cube->cam.plane.y * sin(Alpha));
+	cube->cam.plane.y = (buffplanex * sin(Alpha)) +
+	(cube->cam.plane.y * cos(Alpha));
 	cube->event.right_arow_pressed = 0;
 	raycasting(cube);
 }
