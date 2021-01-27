d8e2da0eabcefeb60d6fb96f81ebc2f6cf1b7eb4
Jean Vanden Bulck <jeanvandenbulck@MacBook-Pro-de-Jean.local>
2021-01-27 16:36:42 +0100

    norminette03

--- srcs/sprites.c
+++ srcs/sprites.c
@@ -1,19 +1,7 @@
 #include "../include/cube.h"
 
-void	set_sprites_value(t_cube *cube, t_vecteur sprite)
+void	set_sprites_value_bis(t_cube *cube)
 {
-	cube->sprite.campos.x = sprite.x - cube->cam.pos.x;
-	cube->sprite.campos.y = sprite.y - cube->cam.pos.y;
-	cube->sprite.invdet = 1.0 / (cube->cam.plane.x * cube->cam.direction.y
-	- cube->cam.direction.x * cube->cam.plane.y);
-	cube->sprite.transform.x = cube->sprite.invdet * (cube->cam.direction.y
-	* cube->sprite.campos.x - cube->cam.direction.x * cube->sprite.campos.y);
-	cube->sprite.transform.y = cube->sprite.invdet * (-cube->cam.plane.y *
-	cube->sprite.campos.x + cube->cam.plane.x * cube->sprite.campos.y);
-	cube->sprite.centerstripe = (int)((cube->wind.x_res / 2) * (1
-	+ cube->sprite.transform.x / cube->sprite.transform.y));
-	cube->sprite.height = abs((int)(cube->wind.y_res /
-	cube->sprite.transform.y));
 	cube->sprite.starty = -cube->sprite.height / 2 + cube->wind.y_res / 2;
 	if (cube->sprite.starty < 0)
 		cube->sprite.starty = 0;
@@ -29,6 +17,23 @@ void	set_sprites_value(t_cube *cube, t_vecteur sprite)
 	if (cube->sprite.endx >= cube->wind.x_res)
 		cube->sprite.endx = cube->wind.x_res - 1;
 	cube->sprite.stripe = cube->sprite.startx;
+}
+
+void	set_sprites_value(t_cube *cube, t_vecteur sprite)
+{
+	cube->sprite.campos.x = sprite.x - cube->cam.pos.x;
+	cube->sprite.campos.y = sprite.y - cube->cam.pos.y;
+	cube->sprite.invdet = 1.0 / (cube->cam.plane.x * cube->cam.direction.y
+	- cube->cam.direction.x * cube->cam.plane.y);
+	cube->sprite.transform.x = cube->sprite.invdet * (cube->cam.direction.y
+	* cube->sprite.campos.x - cube->cam.direction.x * cube->sprite.campos.y);
+	cube->sprite.transform.y = cube->sprite.invdet * (-cube->cam.plane.y *
+	cube->sprite.campos.x + cube->cam.plane.x * cube->sprite.campos.y);
+	cube->sprite.centerstripe = (int)((cube->wind.x_res / 2) * (1
+	+ cube->sprite.transform.x / cube->sprite.transform.y));
+	cube->sprite.height = abs((int)(cube->wind.y_res /
+	cube->sprite.transform.y));
+	set_sprites_value_bis(cube);
 }
 
 void	sprite_drawing_bis(t_cube *cube)
